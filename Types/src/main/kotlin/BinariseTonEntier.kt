aaf58fecb0af76c61e5eaad506850af4f3db668c
Emmanuel Kouassi <emmanuelkou06@gmail.com>
2024-09-02 19:47:40 -0400

    4

---
+++ Types/src/main/kotlin/BinariseTonEntier.kt
@@ -0,0 +1,12 @@
+package org.kouassi
+
+fun main(array: Array<Int>){
+    println(intABinaire("2"))
+}
+fun intABinaire(chain: String){
+
+    var chaine: String=chain
+    var num:Int = chaine.toInt()
+    var binaire = num.
+    return println(num)
+}
